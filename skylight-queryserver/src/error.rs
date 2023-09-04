06f01274a190166ca4042ab48f9283a94fdffcad
itstolf <140963545+itstolf@users.noreply.github.com>
2023-09-04 12:05:55 -0700

    anyhow -> Box<Error>

--- skylight-queryserver/src/error.rs
+++ skylight-queryserver/src/error.rs
@@ -1,13 +1,13 @@
 pub enum Error {
     Status(axum::http::StatusCode, String),
-    Anyhow(anyhow::Error),
+    BoxError(Box<dyn std::error::Error>),
 }
 
 impl axum::response::IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         match self {
             Error::Status(code, reason) => (code, reason),
-            Error::Anyhow(err) => (
+            Error::BoxError(err) => (
                 axum::http::StatusCode::INTERNAL_SERVER_ERROR,
                 format!("{}", err),
             ),
@@ -18,9 +18,9 @@ impl axum::response::IntoResponse for Error {
 
 impl<E> From<E> for Error
 where
-    E: Into<anyhow::Error>,
+    E: Into<Box<dyn std::error::Error>>,
 {
     fn from(err: E) -> Self {
-        Self::Anyhow(err.into())
+        Self::BoxError(err.into())
     }
 }
