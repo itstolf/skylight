f399ba102b4b132d427e92db136ad8d5314205cc
itstolf <140963545+itstolf@users.noreply.github.com>
2023-09-11 13:09:33 -0700

    clean up client code

--- skylight-web/src/lib/client.ts
+++ skylight-web/src/lib/client.ts
@@ -22,83 +22,25 @@ function makeURLSearchParams(args: Record<string, string | string[] | null>): UR
 	return q;
 }
 
-async function call<R>(
+function makeURL(endpoint: string, args: Record<string, string | string[] | null>): string {
+	return `${HOST}/_/${endpoint}?${makeURLSearchParams(args)}`;
+}
+
+async function unaryCall<R>(
 	endpoint: string,
 	args: Record<string, string | string[] | null>,
 	init?: RequestInit
 ): Promise<R> {
-	return await throwForStatus(
-		await fetch(`${HOST}/_/${endpoint}?${makeURLSearchParams(args)}`, init)
-	).json();
-}
-
-export async function whois(
-	actors: string[],
-	init?: RequestInit
-): Promise<Record<string, { alsoKnownAs: string[]; did: string }>> {
-	return (
-		await call<{ whois: Record<string, { alsoKnownAs: string[]; did: string }> }>(
-			'whois',
-			{ actor: actors },
-			init
-		)
-	).whois;
-}
-
-export async function neighborhood(
-	dids: string[],
-	ignoreDids: string[] = [],
-	init?: RequestInit
-): Promise<{ n: string[]; e: number[][]; t: number }> {
-	return await call('neighborhood', { did: dids, ignoreDid: ignoreDids }, init);
-}
-
-export async function akas(dids: string[], init?: RequestInit): Promise<Record<string, string[]>> {
-	if (dids.length == 0) {
-		return {};
-	}
-	const promises = [];
-	const CHUNK_SIZE = 100;
-	for (let i = 0; i < dids.length; i += CHUNK_SIZE) {
-		promises.push(
-			call<{ akas: Record<string, string[]> }>(
-				'akas',
-				{
-					did: dids.slice(i, i + CHUNK_SIZE)
-				},
-				init
-			)
-		);
-	}
-	let r = {};
-	for (const resp of await Promise.all(promises)) {
-		r = { ...r, ...resp.akas };
-	}
-	return r;
-}
-
-export async function mutuals(dids: string[], init?: RequestInit): Promise<string[]> {
-	return (await call<{ mutuals: string[] }>('mutuals', { did: dids }, init)).mutuals;
+	return await throwForStatus(await fetch(makeURL(endpoint, args), init)).json();
 }
 
 const TEXT_DECODER = new TextDecoder();
-
-export async function* paths(
-	sourceDid: string,
-	targetDid: string,
-	ignoreDids: string[] = [],
+async function* streamingCall<R>(
+	endpoint: string,
+	args: Record<string, string | string[] | null>,
 	init?: RequestInit
-): AsyncIterable<string[]> {
-	const resp = throwForStatus(
-		await fetch(
-			`${HOST}/_/paths?${makeURLSearchParams({
-				sourceDid: sourceDid,
-				targetDid: targetDid,
-				ignoreDid: ignoreDids
-			})}`,
-			init
-		)
-	);
+): AsyncIterable<R> {
+	const resp = throwForStatus(await fetch(makeURL(endpoint, args), init));
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 	const reader = resp.body!.getReader();
 	let buf = '';
@@ -120,5 +62,73 @@ export async function* paths(
 		for (const part of parts) {
 			yield JSON.parse(part);
 		}
+	}
+}
+
+export async function whois(
+	actors: string[],
+	init?: RequestInit
+): Promise<Record<string, { alsoKnownAs: string[]; did: string }>> {
+	return (
+		await unaryCall<{ whois: Record<string, { alsoKnownAs: string[]; did: string }> }>(
+			'whois',
+			{ actor: actors },
+			init
+		)
+	).whois;
+}
+
+export async function neighborhood(
+	dids: string[],
+	ignoreDids: string[] = [],
+	init?: RequestInit
+): Promise<{ n: string[]; e: number[][]; t: number }> {
+	return await unaryCall('neighborhood', { did: dids, ignoreDid: ignoreDids }, init);
+}
+
+export async function akas(dids: string[], init?: RequestInit): Promise<Record<string, string[]>> {
+	if (dids.length == 0) {
+		return {};
+	}
+	const promises = [];
+	const CHUNK_SIZE = 100;
+	for (let i = 0; i < dids.length; i += CHUNK_SIZE) {
+		promises.push(
+			unaryCall<{ akas: Record<string, string[]> }>(
+				'akas',
+				{
+					did: dids.slice(i, i + CHUNK_SIZE)
+				},
+				init
+			)
+		);
+	}
+	let r = {};
+	for (const resp of await Promise.all(promises)) {
+		r = { ...r, ...resp.akas };
+	}
+	return r;
+}
+
+export async function mutuals(dids: string[], init?: RequestInit): Promise<string[]> {
+	return (await unaryCall<{ mutuals: string[] }>('mutuals', { did: dids }, init)).mutuals;
+}
+
+export async function* paths(
+	sourceDid: string,
+	targetDid: string,
+	ignoreDids: string[] = [],
+	init?: RequestInit
+): AsyncIterable<string[]> {
+	for await (const part of streamingCall<string[]>(
+		'paths',
+		{
+			sourceDid: sourceDid,
+			targetDid: targetDid,
+			ignoreDid: ignoreDids
+		},
+		init
+	)) {
+		yield part;
 	}
 }
